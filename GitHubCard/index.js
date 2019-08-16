53a22eb7edeb0bfc3e42b2e4bbcd3dd670fabf37
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-08-15 20:31:45 -0400

    completed the first step in js file

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,12 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const mainCard = document.querySelector('.cards')
+axios.get('https://api.github.com/users/dangus1924')
+.then(res => {
+  console.log(res.data);
+  mainCard.appendChild(cardCreator(res.data));
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
